978219d874ca8ddca2f2cc0356a4cd1d895ac2e8
kielGavanez <130537228+kielGavanez@users.noreply.github.com>
2023-06-04 18:48:40 -0700

    done destinction for every pages

--- resources/views/Animation.blade.php
+++ resources/views/Animation.blade.php
@@ -3,4 +3,7 @@
 @section('content')
 <h1>This is my Animation Page </h1>
 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda soluta, accusamus nihil quidem repudiandae officia laborum, explicabo impedit sint sed pariatur nemo velit sapiente, corrupti distinctio eum provident facilis deleniti?</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda soluta, accusamus nihil quidem repudiandae officia laborum, explicabo impedit sint sed pariatur nemo velit sapiente, corrupti distinctio eum provident facilis deleniti?</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda soluta, accusamus nihil quidem repudiandae officia laborum, explicabo impedit sint sed pariatur nemo velit sapiente, corrupti distinctio eum provident facilis deleniti?</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda soluta, accusamus nihil quidem repudiandae officia laborum, explicabo impedit sint sed pariatur nemo velit sapiente, corrupti distinctio eum provident facilis deleniti?</p>
 @endsection
--- resources/views/Tasks.blade.php
+++ resources/views/Tasks.blade.php
@@ -3,4 +3,8 @@
 @section('content')
 <h1>This is my Tasks Page </h1>
 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda soluta, accusamus nihil quidem repudiandae officia laborum, explicabo impedit sint sed pariatur nemo velit sapiente, corrupti distinctio eum provident facilis deleniti?</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda soluta, accusamus nihil quidem repudiandae officia laborum, explicabo impedit sint sed pariatur nemo velit sapiente, corrupti distinctio eum provident facilis deleniti?</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda soluta, accusamus nihil quidem repudiandae officia laborum, explicabo impedit sint sed pariatur nemo velit sapiente, corrupti distinctio eum provident facilis deleniti?</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda soluta, accusamus nihil quidem repudiandae officia laborum, explicabo impedit sint sed pariatur nemo velit sapiente, corrupti distinctio eum provident facilis deleniti?</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda soluta, accusamus nihil quidem repudiandae officia laborum, explicabo impedit sint sed pariatur nemo velit sapiente, corrupti distinctio eum provident facilis deleniti?</p>
 @endsection
